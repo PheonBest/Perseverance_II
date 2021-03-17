722254eb2aff56ab13677aba3b38f15ef90550f5
Thomas Ethève <thomas.etheve@insa-lyon.fr>
2021-03-17 21:46:33 +0100

    Update Robot.java

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -13,13 +13,17 @@ public class Robot extends Avatar {
     private double batterie;
     
     // Trajectoire
+    /* La classe dimension est une classe qui comporte comme attribut une hauteur et une largeur,
+     * elle est utilisée ici pour référencer la position d'un point à atteindre sur la carte par rapport au robot.
+     * Pour aller d'un point à un autre sur la carte, on utilise une liste de cases ciblées, appelées but (à atteindre).*/ 
+     
     private LinkedList<Dimension> but = new LinkedList<Dimension>();
     
     //  Position du robot dans la matrice de la carte
     private int row = 0;
     private int column = 0;
     
-    // Position du robot sur la carte
+    // Position réélle du robot sur la carte, dont l'origine se rouve en haut à gauche
     private int xFictif = 0;
     private int yFictif = 0;
     
@@ -33,22 +37,25 @@ public class Robot extends Avatar {
         this.sante = sante;
         this.batterie = batterie;
     }
+    
+    // Place un robot neuf sur la carte
     public Robot(ArrayList<ArrayList<Image>> image, int x, int y) {
         this(Options.SANTE_MAX, Options.BATTERIE_MAX, 0, Options.JOUEUR_DUREE_ANIMATION, image, new int[] {0,0}, x, y, .0, 0, 0, .0);
     }
 
-    public void definirBut(LinkedList<Dimension> but) {
-        this.but = but;
+    public void definirBut(LinkedList<Dimension> liste) {
+        this.but = liste;
         definirDirection(but.getFirst());
-        animationIndex = 2;
+        animationIndex = 2; //  Image qui montre le robot marcher 
     }
 
-    private void definirDirection(Dimension but) {
-        float angle = (float) Math.atan2(but.getHeight() - yFictif, but.getWidth() - xFictif);
+    private void definirDirection(Dimension caseCiblee) {
+        float angle = (float) Math.atan2(caseCiblee.getHeight() - yFictif, caseCiblee.getWidth() - xFictif);
         //System.out.println(Math.toDegrees(angle));
+        
         dx = (int) (Math.cos(angle)*10); // 10 est la vitesse (pixel/itération de la boucle du jeu)
         dy = (int) (Math.sin(angle)*10);
-        animationIndex = 2; // Marcher
+        animationIndex = 2; // Image qui montre le robot marcher 
     }
 
     @Override
@@ -59,14 +66,14 @@ public class Robot extends Avatar {
         if (!but.isEmpty()) {
             xFictif += dx;
             yFictif += dy;
-            Dimension d = but.getFirst(); // On va jusqu'au centre de l'hexagone
+            Dimension d = but.getFirst(); // On prend une case à cibler 
             if (Math.abs(xFictif - d.getWidth()) < Options.JOUERUR_TOLERANCE_DEPLACEMENT)
                 dx = (int) d.getWidth() - xFictif;
             if (Math.abs(yFictif - d.getHeight()) < Options.JOUERUR_TOLERANCE_DEPLACEMENT)
                 dy = (int) d.getHeight() - yFictif;
             if (dx == 0 && dy == 0) {
                 but.removeFirst();
-                if (but.isEmpty()) // Si il ne reste plus de cases à parcourir
+                if (but.isEmpty()) // Si il ne reste plus de cases à parcourir, le robot est arrivé à la case demandée
                     animationIndex = 0; // On passe l'animation du joueur en mode "pause" (Idle)
                 else {
                     definirDirection(but.getFirst());
