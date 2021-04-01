3df6ad463092fb1a5ed19c5c5a0eb2cab23e4154
agatheflower <agathe.martet@insa-lyon.fr>
2021-04-01 15:16:47 +0200

    symbole

---
+++ src/main/java/Symbole.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.BasicStroke;
+import java.awt.geom.AffineTransform;
+import java.awt.Image;
+
+public class Symbole extends Polygon {
+	
+		private TypeSymbole type;
+		private Image image;
+	
+	public Symbole(TypeSymbole type){
+		this.type=type;
+		//this.image=getImage();
+	}
+
+	//Dans Symbole:On rajoute la méthode
+	public void majSymbole(Image img) {	
+		this.image = img;
+	}
+
+
+	
+	
+}
+
