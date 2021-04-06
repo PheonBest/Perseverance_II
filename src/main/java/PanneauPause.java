087c7a55c2afee97edc8034eed370790e72c930d
agatheflower <agathe.martet@insa-lyon.fr>
2021-04-06 10:22:13 +0200

    RETOUR PAUSE

--- src/main/java/PanneauPause.java
+++ src/main/java/PanneauPause.java
@@ -2,38 +2,54 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class PanneauPause extends JDialog {
+public class PanneauPause extends JDialog implements ActionListener {
 	private int lx=600;
 	private int ly=600;
 	private int x0=250;
 	private int y0=50;
 	private JPanel rouge;
 	private JLabel monImage;
+	private JButton retour;
 	
 	public PanneauPause(){
 		super();
         setLayout(null);
-        setSize(lx,ly);
+        setSize(lx,ly+40);
         setLocation(x0,y0);
         setLayout(null);
-        setTitle("panneau pause");
         
+        setTitle("panneau pause");
+        // fenetre modale 
+        this.setModal(true);
+        //fond rouge
         rouge= new JPanel();
         rouge.setLayout(null);
 		rouge.setBounds(0,0,lx,ly);
 		rouge.setBackground(Color.red);
 		rouge.setVisible(true);
 		this.add(rouge);
-        
+        //image pause
         monImage = new JLabel(new ImageIcon("./res/images/pause.png"));
         monImage.setVisible(true);
 		monImage.setBounds(lx/2-500/2,150,500,250);
-		
-	
 		rouge.add(monImage);
+		//retour
+		retour = new JButton("RETOUR");
+        retour.setLocation(lx-120,ly-60);
+        retour.setSize(100,50);
+        retour.setBackground(Color.green);
+        retour.addActionListener(this);
+        this.add(retour);
 		
-		this.setModal(true);
+		
+		
 	}
+		public void actionPerformed(ActionEvent e){
+			this.setVisible(false);
+        
+    }
+	
+	
 /*
 	public void showDialog() {
 
