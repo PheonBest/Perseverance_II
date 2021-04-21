39b3d10af80f9c43ba61f0a770021d1108d98430
agatheflower <agathe.martet@insa-lyon.fr>
2021-04-21 09:26:22 +0200

    Update RegleJeu.java
    j'ai enelvé les guillemets à l'interieur des guillemets sinon ca compilait pas

--- src/main/java/RegleJeu.java
+++ src/main/java/RegleJeu.java
@@ -79,7 +79,7 @@ public class RegleJeu extends JFrame implements ActionListener {
 			+"<hr>"
             +"<i><u>SYMBOLE</u></i> <br>"
             +"<br>"
-            +"Les sources de vies bactériennes, minerais, objets et caisses de ravitaillement sont présents sous forme de symboles “?”. Pour découvrir de quel symbole il s’agit, vous allez devoir les scanner à l’aide de l’outil scanner puis utiliser le grappin pour les extraire. Ces deux outils sont disponibles sur la gauche de l’écran de jeu et vous demanderont une concentration accrue lorsque vous les utiliserez. </center>");
+            +"Les sources de vies bactériennes, minerais, objets et caisses de ravitaillement sont présents sous forme de symboles ? . Pour découvrir de quel symbole il s’agit, vous allez devoir les scanner à l’aide de l’outil scanner puis utiliser le grappin pour les extraire. Ces deux outils sont disponibles sur la gauche de l’écran de jeu et vous demanderont une concentration accrue lorsque vous les utiliserez. </center>");
             
 			texteRegle.setFont(new Font("Serif", Font.BOLD, 15));
 			texteRegle.setForeground(Color.BLACK);
